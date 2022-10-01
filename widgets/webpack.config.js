0d6a3262a256d2f8bb5e9030927864f2635b49b0
DawnGate <lam99quyen@gmail.com>
2022-10-01 14:03:42 +0700

    init project and step 2 complete

--- widgets/webpack.config.js
+++ widgets/webpack.config.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'e-widgets.v1.min.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+    filename: "e-widgets.v1.min.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
+    allowedHosts: "all",
     port: 9000,
   },
   watchOptions: {
@@ -23,8 +24,8 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: {presets: ["@babel/preset-env", "@babel/preset-react"]}
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] },
       },
-    ]
-  }
+    ],
+  },
 };
